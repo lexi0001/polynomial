9f3fa80d79d9989213fe6fade1cc69cb19572167
Alexandra Rodriguez <arod27@bu.edu>
2024-01-28 16:47:31 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, xVal):
+        return xVal
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, xVal):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, xVal):
+        return self.p1.evaluate(xVal) + self.p2.evaluate(xVal)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -41,6 +50,9 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, xVal):
+        return self.p1.evaluate(xVal) * self.p2.evaluate(xVal)
 
 class Div:
     def __init__(self, p1, p2):
@@ -51,6 +63,13 @@ class Div:
         repr_p1 = repr(self.p1) if not isinstance(self.p1, Add) else "( " + repr(self.p1) + " )"
         repr_p2 = repr(self.p2) if not isinstance(self.p2, Add) else "( " + repr(self.p2) + " )"
         return repr_p1 + " / " + repr_p2
+    
+    def evaluate(self, xVal):
+        divisor = self.p2.evaluate(xVal)
+        if divisor != 0:
+            return self.p1.evaluate(xVal) / divisor
+        else:
+            raise ZeroDivisionError()
 
 class Sub:
     def __init__(self, p1, p2):
@@ -64,6 +83,12 @@ class Sub:
         repr_p1 = repr(self.p1)
         repr_p2 = wrapper(self.p2)
         return repr_p1 + " - " + repr_p2
+    
+    def evaluate(self, xVal):
+        return self.p1.evaluate(xVal) - self.p2.evaluate(xVal)
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
